bdd237234d4ae1f52d7f7c3a0e2a31d7859f65bb
hesic73 <hesicheng20@gmail.com>
2024-07-12 22:20:05 -0700

    minor updates

--- static/js/base.js
+++ static/js/base.js
@@ -20,3 +20,56 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function scrollToBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
+
+
+/**
+ * 
+ * @param {number} novelId 
+ * @param {number} chapterId 
+ */
+function addBookmark(novelId, chapterId = null) {
+
+    console.log('Bookmark button clicked');  // Log to confirm the function is called
+    console.log('Novel ID:', novelId);
+    console.log('Chapter ID:', chapterId);
+
+    const token = localStorage.getItem('access_token');
+    const requestBody = {
+        novel_id: novelId,
+    };
+    if (chapterId !== null) {
+        requestBody.chapter_id = chapterId;
+    }
+
+    fetch('/api/bookmark/', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('书签已更新');
+            } else {
+                return response.json().then(error => {
+                    throw new Error(error.detail || 'Unknown error');
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error updating bookmark:', error.message);
+            alert('更新书签时出错: ' + error.message);
+        });
+
+}
--- static/js/chapter.js
+++ static/js/chapter.js
@@ -18,12 +18,3 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
     }
 });
-
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-function scrollToBottom() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-}
